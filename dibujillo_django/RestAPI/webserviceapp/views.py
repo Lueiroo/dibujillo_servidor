b820511e57dcd709dfff9a98292f6559cf30ce82
crismorber <113433738+crismorber@users.noreply.github.com>
2023-02-02 12:30:07 +0100

    Subida archivos fallidos Cris Maceiras

--- dibujillo_django/RestAPI/webserviceapp/views.py
+++ dibujillo_django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Usuario
+
+import json
+import jwt
 
 # Create your views here.
+
+@csrf_exempt
+def invitado(request):
+	if request.method != 'POST':
+		return None
+
+	try:
+		peticion = json.loads(request.body)
+	except json.decoder.JSONDecodeError:
+		return JsonResponse({'error':'JSON invalido'}, status=400)
+
+	invitado = Usuario()
+	nomInvitado = peticion['name']
+
+	if (nomInvitado == ""):
+		return JsonResponse({'error':'Faltan parametros'}, status=400)
+
+	invitado2 = Usuario.objects.filter(nombre = nomInvitado).exists()
+
+	if (invitado2 is False):
+		invitado.nombre = nomInvitado
+		payload = {
+			'username':invitado.nombre
+		}
+		secret = 'muysecreto'
+		token = jwt.encode(payload, secret, algorithm='HS256')
+		invitado.token = token
+		invitado.save()
+		return JsonResponse({"sessionToken":token},status=200)
+	else:
+		return JsonResponse({'error':'Nombre ya existe'}, status=400)
+
+
