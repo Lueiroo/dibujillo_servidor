d871bbd50b8bff48c954537b0239c5b2f5d2812e
Cristina Moreno <cmorenob@fpcoruna.afundacion.org>
2023-02-02 11:36:04 +0100

    ddj

--- dibujillo_django/RestAPI/webserviceapp/views.py
+++ dibujillo_django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Usuario
+import json
+
 
 # Create your views here.
+@csrf_exempt 
+def inicio_sesion(request):
+	if request.method !=POST:
+		return None
+
+	json_peticion = json.loads(request.body)
+	
