bc83d4ac8740f7d2b8a1fc8595a67c3d48e0facb
crismorber <113433738+crismorber@users.noreply.github.com>
2023-02-02 10:18:34 +0100

    Actualizada base de datos v1.1

--- Base de datos dibujillo.sql
+++ Base de datos dibujillo.sql
@@ -9,6 +9,8 @@ CREATE TABLE usuario(
 
 CREATE TABLE partida(
     codigo int primary key not null,
+    historia varchar(500) not null,
+    createdAt date not null,
     publica boolean not null,
     token_usuario varchar(100) not null
 );
@@ -41,12 +43,6 @@ CREATE TABLE valora(
     puntuacion int not null
 );
 
-CREATE TABLE contiene(
-    id int primary key not null,
-    id_dibujo int not null,
-    codigo_partida int not null,
-    historia varchar(100) not null
-);
 
 ALTER TABLE partida ADD CONSTRAINT fk_usuario_partida_crea FOREIGN KEY (token_usuario) REFERENCES usuario(token);
 ALTER TABLE participa ADD CONSTRAINT fk_usuario_participa FOREIGN KEY (token_usuario) REFERENCES usuario(token);
@@ -55,9 +51,6 @@ ALTER TABLE participa ADD CONSTRAINT fk_partida_participa FOREIGN KEY (codigo_pa
 ALTER TABLE dibujo ADD CONSTRAINT fk_usuario_dibujo_crea FOREIGN KEY (token_usuario) REFERENCES usuario(token);
 ALTER TABLE valora ADD CONSTRAINT fk_usuario_valora FOREIGN KEY (token_usuario) REFERENCES usuario(token);
 ALTER TABLE valora ADD CONSTRAINT fk_dibujo_valora FOREIGN KEY (id_dibujo) REFERENCES dibujo(id);
-
-ALTER TABLE contiene ADD CONSTRAINT fk_contiene_dibujo FOREIGN KEY (id_dibujo) REFERENCES dibujo(id);
-ALTER TABLE contiene ADD CONSTRAINT fk_partida_contiene FOREIGN KEY (codigo_partida) REFERENCES partida(codigo);
 
 ALTER TABLE comentario ADD CONSTRAINT fk_usuario_comentario FOREIGN KEY (token_usuario) REFERENCES usuario(token);
 ALTER TABLE comentario ADD CONSTRAINT fk_dibujo_comentario FOREIGN KEY (id_dibujo) REFERENCES dibujo(id);
