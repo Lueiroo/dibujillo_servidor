fb0c945bc5866d29409976f13556942274c39042
cristina <mmaceirass@fpcoruna.afundacion.org>
2023-02-02 12:16:06 +0100

    Primer endpoint funciona correctamente

--- dibujillo_django/RestAPI/webserviceapp/views.py
+++ dibujillo_django/RestAPI/webserviceapp/views.py
@@ -18,14 +18,15 @@ def invitado(request):
 	except json.decoder.JSONDecodeError:
 		return JsonResponse({'error':'JSON invalido'}, status=400)
 
-	if not all(k in peticion for k in("name")):
-		return JsonResponse({'error':'Faltan parametros'}, status=400)
-
 	invitado = Usuario()
 	nomInvitado = peticion['name']
-	invitado2 = Usuario.objects.filter(nombre = nomInvitado)
-	print(invitado2)
-	if (nomInvitado != invitado2[0]):
+
+	if (nomInvitado == ""):
+		return JsonResponse({'error':'Faltan parametros'}, status=400)
+
+	invitado2 = Usuario.objects.filter(nombre = nomInvitado).exists()
+
+	if (invitado2 is False):
 		invitado.nombre = nomInvitado
 		payload = {
 			'username':invitado.nombre
