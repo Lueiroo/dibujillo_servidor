a17d7be0ae7cff79bb618af568a755f30065ead5
cristinamaceiras <113434199+cristinamaceiras@users.noreply.github.com>
2023-02-06 12:43:45 +0100

    cambios en el tamaño de atributos y otros errores

--- Base de datos dibujillo.sql
+++ Base de datos dibujillo.sql
@@ -4,40 +4,41 @@ CREATE TABLE usuario(
     token varchar(256) primary key not null,
     nombre varchar(20) not null,
     email varchar(50) null,
-    contrasena varchar(20) null
+    contrasena varchar(256) null
 );
 
 CREATE TABLE partida(
-    codigo int primary key not null,
-    historia varchar(500) not null,
-    createdAt date not null,
-    token_usuario varchar(100) not null
+    codigo varchar(3) primary key not null,
+    historia varchar(500) null,
+    createdAt datetime not null,
+    token_usuario varchar(256) not null
 );
 
 CREATE TABLE participa(
-    id int primary key not null,
+    id int auto_increment primary key not null,
     token_usuario varchar(100) not null,
     codigo_partida int not null
 );
 
 CREATE TABLE dibujo(
-    id int primary key not null,
+    id int auto_increment primary key not null,
     fecha date null, /*si tiene fecha ta publicao*/
-    link varchar(100) not null,
-    token_usuario varchar(100) not null
+    link varchar(400) not null,
+    token_usuario varchar(256) not null,
+    codigo_partida int not null
 );
 
 CREATE TABLE comentario(
-    id int primary key not null,
+    id int auto_increment primary key not null,
     comentario varchar(1000) not null,
-    token_usuario varchar(100) not null,
+    token_usuario varchar(256) not null,
     id_dibujo int not null
 );
 
 
 CREATE TABLE valora(
-    id int primary key not null,
-    token_usuario varchar(100) not null,
+    id int auto_increment primary key not null,
+    token_usuario varchar(256) not null,
     id_dibujo int not null,
     puntuacion int not null
 );
@@ -53,3 +54,5 @@ ALTER TABLE valora ADD CONSTRAINT fk_dibujo_valora FOREIGN KEY (id_dibujo) REFER
 
 ALTER TABLE comentario ADD CONSTRAINT fk_usuario_comentario FOREIGN KEY (token_usuario) REFERENCES usuario(token);
 ALTER TABLE comentario ADD CONSTRAINT fk_dibujo_comentario FOREIGN KEY (id_dibujo) REFERENCES dibujo(id);
+
+ALTER TABLE dibujo ADD CONSTRAINT fk_dibujo_partida FOREIGN KEY (codigo_partida) REFERENCES partida(codigo);
