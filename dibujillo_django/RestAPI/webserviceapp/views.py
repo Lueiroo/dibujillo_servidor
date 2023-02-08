4620f6196fc01907c878b1c257a49d0602dffbdb
crismorber <113433738+crismorber@users.noreply.github.com>
2023-02-08 21:23:36 +0100

    Update views.py

--- dibujillo_django/RestAPI/webserviceapp/views.py
+++ dibujillo_django/RestAPI/webserviceapp/views.py
@@ -8,7 +8,9 @@ import json
 import jwt
 
 # Create your views here.
-@csrf_exempt 
+
+
+@csrf_exempt
 def invitado(request):
 	if request.method != 'POST':
 		return None
@@ -16,78 +18,79 @@ def invitado(request):
 	try:
 		peticion = json.loads(request.body)
 	except json.decoder.JSONDecodeError:
-		return JsonResponse({'error':'JSON invalido'}, status=400)
+		return JsonResponse({'error': 'JSON invalido'}, status=400)
 
 	invitado = Usuario()
 	nomInvitado = peticion['name']
 
 	if (nomInvitado == ""):
-		return JsonResponse({'error':'Faltan parametros'}, status=400)
+		return JsonResponse({'error': 'Faltan parametros'}, status=400)
 
-	invitado2 = Usuario.objects.filter(nombre = nomInvitado).exists()
+	invitado2 = Usuario.objects.filter(nombre=nomInvitado).exists()
 
 	if (invitado2 is False):
 		invitado.nombre = nomInvitado
 		payload = {
-			'username':invitado.nombre
+			'username': invitado.nombre
 		}
 		secret = 'muysecreto'
 		token = jwt.encode(payload, secret, algorithm='HS256')
 		invitado.token = token
 		invitado.save()
-		return JsonResponse({"sessionToken":token},status=201)
+		return JsonResponse({"sessionToken": token}, status=201)
 	else:
-		return JsonResponse({'error':'Nombre ya existe'}, status=400)
+		return JsonResponse({'error': 'Nombre ya existe'}, status=400)
 
-@csrf_exempt 	
+
+@csrf_exempt
 def login(request):
-	if request.method !='POST':
+	if request.method != 'POST':
 		return None
-	
+
 	try:
 		peticion = json.loads(request.body)
 	except json.decoder.JSONDecodeError:
-		return JsonResponse({'error':'JSON invalido'}, status=400)
-	
-	usuario=Usuario()
-	nameOrEmail=peticion['nameOrEmail']
-	password=peticion['password']
+		return JsonResponse({'error': 'JSON invalido'}, status=400)
+
+	usuario = Usuario()
+	nameOrEmail = peticion['nameOrEmail']
+	password = peticion['password']
 	usuario.set_password(password)
 
 	if (nameOrEmail == ''):
-		return JsonResponse({'error':'Faltan parámetros'}, status=400)
+		return JsonResponse({'error': 'Faltan parámetros'}, status=400)
 
-	name1 = Usuario.objects.filter(nombre = nameOrEmail).exists()
-	email1 = Usuario.objects.filter(email = nameOrEmail).exists()
+	name1 = Usuario.objects.filter(nombre=nameOrEmail).exists()
+	email1 = Usuario.objects.filter(email=nameOrEmail).exists()
 
 	if (name1 is True):
-		usuario2=Usuario.objects.get(nombre = nameOrEmail)
+		usuario2 = Usuario.objects.get(nombre=nameOrEmail)
 		pass
 	elif (email1 is True):
-		usuario2=Usuario.objects.get(email = nameOrEmail)
-		pass		
+		usuario2 = Usuario.objects.get(email=nameOrEmail)
+		pass
 	else:
-		return JsonResponse({'error':'El susuario no existe'}, status=400)
-		
+		return JsonResponse({'error': 'El susuario no existe'}, status=400)
+
 	if check_password(password, usuario2.contraseña):
 		secret = 'muysecreto'
 		token = jwt.encode(payload, secret, algorithm='HS256')
 		usuario.token = token
 		usuario.save()
-		return JsonResponse({"sessionToken":token},status=200)
+		return JsonResponse({"sessionToken": token}, status=200)
 	else:
-		return JsonResponse({'error':'La contraseña es incorrecta'}, status=401)
-	
-@csrf_exempt 
-def join_game(request,cod):
+		return JsonResponse({'error': 'La contraseña es incorrecta'}, status=401)
+
+
+@csrf_exempt
+def join_game(request, cod):
     if request.method != 'POST':
         return None
-
 
     try:
        session_token = request.headers.get('sessionToken')
     except Exception:
-        return JsonResponse({'error':'SessionToken does no exist'}, status=401)
+        return JsonResponse({'error': 'SessionToken does no exist'}, status=401)
     try:
         user = Usuario.objects.get(token=session_token)
     except Usuario.DoesNotExist:
@@ -98,6 +101,9 @@ def join_game(request,cod):
     if not cod:
         return JsonResponse({'error': 'Code does not exist'}, status=400)
 
+    partida = Partida.objects.all()
+    for p in partida:
+        print(p.codigo)
 
     try:
         game = Partida.objects.get(codigo=cod)
