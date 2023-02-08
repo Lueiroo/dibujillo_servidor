c53e92b89705cb0bf59de5d07668968ee8af5593
crismorber <113433738+crismorber@users.noreply.github.com>
2023-02-08 13:49:49 +0100

    Cambios varios

--- dibujillo_django/RestAPI/RestAPI/settings.py
+++ dibujillo_django/RestAPI/RestAPI/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
